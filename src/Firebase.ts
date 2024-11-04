e1c286c57f581b105751703c13a9ca31c3e8277f
Kuronoma <dlovito3@gmail.com>
2024-11-04 06:05:31 -0500

    Agragacion env

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -9,13 +9,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCcDCbQFqjgZ-gsTTJEXJaQiepCvkA04aY",
-  authDomain: "kuronoma-851c5.firebaseapp.com",
-  projectId: "kuronoma-851c5",
-  storageBucket: "kuronoma-851c5.firebasestorage.app",
-  messagingSenderId: "852327038982",
-  appId: "1:852327038982:web:b477caf822287e305ec387",
-  measurementId: "G-70BML5KTGW"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
